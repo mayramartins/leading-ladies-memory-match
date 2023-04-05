ddab0a7ceff648190912ff5abe116713c22f93e5
mayramartins <mayramartins.in@gmail.com>
2023-04-04 15:40:44 -1000

    html/css fixed

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
   /*----- constants -----*/
-    //const = 
+const images = [
+  'url(imgs/img1.png',
+  'url(imgs/img2.png',
+  'url(imgs/img3.png',
+  'url(imgs/img4.png',
+  'url(imgs/img5.png',
+  'url(imgs/img6.png',
+  'url(imgs/img7.png',
+  'url(imgs/img8.png',
+]
 
 
   /*----- state variables -----*/
-  // let
-
-
+  let flippedCards = false 
+  let firstCard, secondCard;
+  let numberOfClicks = 0;
+  
   /*----- cached elements  -----*/
-
+  const cards = document.querySelectorAll();
 
   /*----- event listeners -----*/
   // addEventListener
 
   /*----- functions -----*/
-  
+intitialize();
+
+function letsPlayBtn {
+
+}
+
+function flipCard {
+
+}
+
+function macthedCards {
+
+}
