a9ffcd3c685a632ad94c29d157b0e7c307f3ea98
mayramartins <mayramartins.in@gmail.com>
2023-04-06 08:22:21 -1000

    currently finishing game logic

--- js/script.js
+++ js/script.js
@@ -11,134 +11,102 @@
 // When the player finds all the matching cards message is displayed: You win the game! Refresh to play again
 // End of the game
 
+const divsArray = Array.from(divs);
+divsArray.sort(() => Math.random() - 0.5);
+const container = document.querySelector('#board');
+divsArray.forEach(div => container.appendChild(div));
 
 /*----- constants -----*/
-const images = [
-  'url(imgs/img1.png',
-  'url(imgs/img2.png',
-  'url(imgs/img3.png',
-  'url(imgs/img4.png',
-  'url(imgs/img5.png',
-  'url(imgs/img6.png',
-  'url(imgs/img7.png',
-  'url(imgs/img8.png',
-]
 
 
 /*----- state variables -----*/
-let matchedCards = 0;
+//let matchedCards = 0;
 let firstCard, secondCard; //Set firstCard and secondCard with undefined value
+let matches;
+
 
 /*----- cached elements  -----*/
 // Get the cards to be clicked by the player
+// Get all my divs from html using the parameter flipcardinner
+// search in all divs and save all divs with class fli-card-inner
+//1
 const cards = document.querySelectorAll('.flip-card-inner');
-  
-  
+
   
   
 /*----- event listeners -----*/
   ///board.forEach.addEventListener('click', flipCard);
-  cards.forEach(card => {
-    card.addEventListener('click', flipCard);
-  });
-  
-  
-  /*----- functions -----*/
-  //intitialize();
-  // function initialize {
-    //}
-    
-    function flipCard(e) {
-    
-      // The player clicks on the first card and first card is selectedflips the first card revealing the image
-      let selectedCard = e.currentTarget;
-      if (firstCard == undefined) {
-        firstCard = selectedCard;
-        selectedCard.style.transform = 'rotateY(180deg)';
-        // The player clicks on the second card and the second card is selecionado 
-      } else if (secondCard == undefined) {
+  // Configure the event listener to listening to the click event and invoke the flipcard
+cards.forEach(card => {
+ card.addEventListener('click', flipCard);
+});
 
-        secondCard = selectedCard;
-        selectedCard.style.transform = 'rotateY(180deg)';
-      } else {
-        let firstImage = firstCard.children[0].children[0].getAttribute('src');
-        let secondImage = secondCard.children[0].children[0].getAttribute('src');
+
+// *----- functions -----*/
+// List of all functions  
+
+intitialize ();
+
+function intitialize() {
+
+}
+
+function render(selectedCard) {
+  selectedCard.style.transform = 'rotateY(180deg)';
+  
+  setTimeout(() => {
+    let firstImage = firstCard.children[0].children[0].getAttribute('src');
+    let secondImage = secondCard.children[0].children[0].getAttribute('src');
+
+    if (firstImage == secondImage) {
+      console.log('Its a Match');
+    } else {
+      firstCard.style.transform = '';
+      secondCard.style.transform = '';
+      console.log('Is Not a Match')
+    }
+
+  firstCard = undefined;
+  secondCard = undefined;
+
+}, 1000);
+} 
+
+function flipCard(e) {
+  console.log('click');
+
+
+
+
+// The player clicks on the first card and first card is selectedflips the first card revealing the image
+let selectedCard = e.currentTarget;
+  if (firstCard == undefined) {
+    firstCard = selectedCard;
+    selectedCard.style.transform = 'rotateY(180deg)';
+// The player clicks on the second card and the second card is selecionado 
+  } else if (secondCard == undefined) {
+    secondCard = selectedCard;
+    selectedCard.style.transform = 'rotateY(180deg)';
+    
+    setTimeout(() => {
+      let firstImage = firstCard.children[0].children[0].getAttribute('src');
+      let secondImage = secondCard.children[0].children[0].getAttribute('src');
         if (firstImage == secondImage) {
           console.log('Its a Match');
         } else {
+          firstCard.style.transform = '';
+          secondCard.style.transform = '';
           console.log('Is Not a Match')
         }
-        firstCard.style.transform = '';
-        secondCard.style.transform = '';
-
+        //reset firstCard and secondCard
         firstCard = undefined;
         secondCard = undefined;
-      }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-//       //selectedCard.parentElement.parentElement.classList.add('flip-card-flipped');
-
-
-//       //firstCard = selectedCard;
-//       //secondCard = selectedCard
-      
-//       if (selectedCard === firstCard) {
-//         selectedCard.classList.toggle('flip');
-//         console.log(selectedCard);
-
-//         if(!firstCard) {
-//           return firstCard = selectedCard;
-      
-//         }
-      
-//       } else if (!secondCard) {
-//     secondCard = selectedCard
-//   }
-
-// }
-
-//function macthedCard() {
+        
+      }, 1000);
+    } 
+  }
   
-//}
-
-//function letsPlayBtn() {
-
-//}
-
-// render()
-
-//function render() {
-// renderBoard();
-// renderNumberOfClicks();
-//}
+// function number of clicks
 
 
+  
